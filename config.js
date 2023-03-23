74f1f1a47087639a4922fd6dde3bdaadbece5876
Tintu K John <tintukjohn@gmail.com>
2023-03-24 03:11:02 +0800

    Sending mail from node

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    user: 'tintukjohn123@gmail.com',
+    clientId: '150860669091-51dale3k533p6lei0tknnh48kgk9p0i4.apps.googleusercontent.com',
+    clientSecret: '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',
+    refreshToken: '1//04otvnWAHqMLACgYIARAAGAQSNwF-L9Ir1pdbQsV6JfrZ3VPraKB97cJvXAhLmpM0r71NfKvo6sPorROwUs_GSN-RQVCoIZ8Sbck'
+    
+}
---
+++ server.js
@@ -0,0 +1,47 @@
+const nodemailer = require('nodemailer')
+const {google} = require('googleapis')
+const config = require('./config.js')
+const OAuth2 = google.auth.OAuth2
+
+const OAuth2_client = new OAuth2(config.clientId, config.clientSecret)
+OAuth2_client.setCredentials({refresh_token : config.refreshToken})
+
+function send_mail(name, recipient) {
+    const accessToken = OAuth2_client.getAccessToken()
+
+
+const transport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        type: 'OAuth2',
+        user: config.user,
+        clientId: config.clientId,
+        clientSecret: config.clientSecret,
+        refreshToken: config.refreshToken,
+        accessToken: accessToken
+    }
+})
+
+const mail_options = {
+    from: `T.I.N.T.U <${config.user}>`,
+    to: recipient,
+    subject: `Testing new email`,
+    text: get_html_message(name)
+}
+
+transport.sendMail(mail_options, function(error, result){
+    if(error){
+        console.log('Error: ', error)
+    } else {
+        console.log('Success: ',result)
+    }
+    transport.close()
+})
+
+}
+
+function get_html_message(name){
+    return ` ${name}! The required result obtained...! `
+}
+
+send_mail('Hello', 'fonop35937@kaudat.com')
